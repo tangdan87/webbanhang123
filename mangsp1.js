6c69a79684df8d677528aa7a1a4832cd6131a3f6
Eric Petter <ericpetter@Erics-MacBook-Air.local>
2020-01-10 12:37:12 +0700

    cm1

---
+++ mangsp1.js
@@ -0,0 +1,72 @@
+<script type="text/javascript">
+        var data = {
+                "items": [
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/km1_27a622ac505244f0bf42e1684eb0c404_medium.jpg",
+			                    "lname": "Nước xả vải AZUMA đậm đặc 3.2L (Hương lavender)",
+			                    "price": "160,000₫"
+			                },
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/tay_moc_500ml_cc45b12a517a472bbac0b2f29b8228b5_medium.jpg",
+			                    "lname": "Tẩy vết nấm mốc",
+			                    "price": "28,000₫"
+			                },
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/bg_azuma_1kg-820_medium.png",
+			                    "lname": "Bột giặt AZUMA SpeepUp hương gió biển 1kg",
+			                    "price": "40,000₫"
+			                },
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/bot_giat_800g_820_902a972c82b3403bb15ef4e7d4cc37cd_medium.jpg",
+			                    "lname": "Bột giặt Sandokkaebi 800g",
+			                    "price": "39,000₫"
+			                },
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/5kg_23828c5a5f0a426db572b05b02c2d01d_medium.jpg",
+			                    "lname": "Bột giặt Sandokkaebi 5kg",
+			                    "price": "210,000₫"
+			                },
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/bg_3_acea4a061536468fa7d5ad388306a704_medium.jpg",
+			                    "lname": "Bột giặt Sandokkaebi 3.3kg",
+			                    "price": "150,000₫"
+			                }
+			                ,
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/bg_azuma_5kg_medium.jpg",
+			                    "lname": "Bột giặt AZUMA SpeepUp hương gió biển 5kg",
+			                    "price": "199,000₫"
+			                }
+			                ,
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/nx_azuma_1.3l_820_medium.png",
+			                    "lname": "Nước xả vải AZUMA đậm đặc 1.3L",
+			                    "price": "65,000₫"
+			                }
+			                ,
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/km_fb1728f7bfb4472b885affa1e9ab6589_medium.jpg",
+			                    "lname": "Nước xả vải AZUMA đậm đặc 3.2L (Hương hồng)",
+			                    "price": "160,000₫"
+			                }
+			                ,
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/untitled-_c21681f6972a402c9f470a2e082f5992_medium.jpg",
+			                    "lname": "Nước giặt xả Sawady 6 trong 1 (Golden Boss) 3,8L",
+			                    "price": "185,000₫"
+			                }
+			                ,
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/tim_c1bb8966756c41cf8d6ce3b559b0be6b_medium.jpg",
+			                    "lname": "Nước giặt xả Sawady 6 trong 1 (Golden Mimosa) 3,8L",
+			                    "price": "185,000₫"
+			                }
+			                ,
+			                {
+			                    "linkimg": "https://product.hstatic.net/1000123703/product/ngx_sawady_golden_fresh_bb021df2f8ea4ba9845145acea598fc7_medium.jpg",
+			                    "lname": "Nước giặt xả Sawady 6 trong 1 (Golden Fresh) 3,8L",
+			                    "price": "185,000₫"
+			                }
+                		] 
+                	}
+    </script>
